a7732067bb79c20473f568f0e9cd127a9b446b5e
jongmee <jp3869@naver.com>
2025-02-21 08:02:41 +0900

    chore: es, batch 모듈 세팅

---
+++ es/src/main/java/org/learn/es/EsApplication.java
@@ -0,0 +1,13 @@
+package org.learn.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EsApplication.class, args);
+  }
+
+}
---
+++ es/src/test/java/org/learn/es/EsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learn.es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
